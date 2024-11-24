16b6bb563b876d5741169e05eec466301ad1c72f
mdalamin123a <md.alamindev90@gmail.com>
2024-11-24 23:07:46 +0600

    41-6-Sign-in-with-google-Open-google-login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import * as firebase from 'firebase/app';
 import firebaseConf from './firebaseConf';
-import { GoogleAuthProvider } from  'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from  'firebase/auth';
 import './App.css';
 
-firebase.initializeApp(firebaseConf)
+firebase.initializeApp(firebaseConf);
+
 function App() {
   
   const provider = new GoogleAuthProvider();
 
+  const handleSignIn = () => {
+    signInWithPopup(getAuth(), provider)
+    .then(result => {
+      const {displayName, email ,photoURL} = result.user;
+      console.log(displayName, email, photoURL);
+    });
+  }
+  console.log(getAuth());
+
   return (
     <div className="App">
-      <button>Sign in</button>
+      <button onClick={handleSignIn}>Sign in</button>
     </div>
   );
 }
