b6460366db31f37e6e6d6b38bd7e25a91c57022c
mdalamin123a <md.alamindev90@gmail.com>
2024-11-24 21:37:27 +0600

    41-4-Create-Simple-Authentication-Project-And-Firebase-Config

---
+++ firebaseConfig.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChI0Rgv89c8QQJTDPLABBGASGpsiFRASA",
+    authDomain: "ema-john-simple-a93fb.firebaseapp.com",
+    projectId: "ema-john-simple-a93fb",
+    storageBucket: "ema-john-simple-a93fb.firebasestorage.app",
+    messagingSenderId: "138760070179",
+    appId: "1:138760070179:web:6648e9e6236cf561e57d96",
+    measurementId: "G-NVQCKQVWH4"
+  };
+
+  export default firebaseConfig;
